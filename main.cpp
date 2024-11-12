460f37961e3580a96b7701b8e26e311693972f0c
Cruceru Ariton Gabriel <cruceruariton25@gmail.com>
2024-11-12 14:25:29 +0200

    123

--- main.cpp
+++ main.cpp
@@ -1,33 +1,66 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+struct problem
+{
+    std::string id;
+    std::string speciality;
+};
+
+struct doctor
+{
+    std::string id;
+    std::string speciality;
+};
+
 int main()
 {
+
     ifstream inFile("input.txt");
 
     int no_problems, no_doctors;
-    string name, speciality;
-    
+
     inFile >> no_problems;
 
+    std::vector <problem> problems(no_problems);
     for (int i = 0; i < no_problems; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        inFile >> problems[i].id;
+        inFile >> problems[i].speciality;
+
     }
 
     inFile >> no_doctors;
-
+    std::vector <doctor>  doctors(no_doctors);
     for (int i = 0; i < no_doctors; i++)
     {
-        inFile >> name;
-        inFile >> speciality;
-        cout << name << ' ' << speciality << '\n';
+        doctor doc;
+
+        inFile >> doc.id;
+        inFile >> doc.speciality;
+
+        doctors.push_back(doc);
     }
 
+    for (auto problem : problems) {
+        bool accepted = false;
+        for (auto doctor : doctors) {
+            if (problem.speciality == doctor.speciality) {
+                accepted = true;
+                break;
+            }
+        }
+        if (accepted) {
+            std::cout << problem.id << " Acceptat" << std::endl;
+        }
+        else {
+            std::cout << problem.id << " Respins" << std::endl;
+        }
+
+    }
     return 0;
 }
